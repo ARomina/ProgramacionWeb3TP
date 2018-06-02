d5cec95e07e82267b84ec05fca83b1757119160d
ARomina <aldanarominabarraza@gmail.com>
2018-06-02 08:17:45 -0300

    Edits

--- ProgramacionWeb3TP/Models/Repositories/Tarea/TareaRepository.cs
+++ ProgramacionWeb3TP/Models/Repositories/Tarea/TareaRepository.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ProgramacionWeb3TP.Models.Repositories.Tarea {
+namespace ProgramacionWeb3TP.Models.Repositories {
     public class TareaRepository {
+    
     }
 }
