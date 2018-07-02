3d7de28e15c860e44f4b9020e83821df07da4361
ARomina <aldanarominabarraza@gmail.com>
2018-07-01 21:19:19 -0300

    Se agrego el captcha al registro

--- ProgramacionWeb3TP/Controllers/HomeController.cs
+++ ProgramacionWeb3TP/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Runtime.Remoting.Contexts;
 using System.Web;
 using System.Web.Mvc;
 using System.Net;
-
+using ProgramacionWeb3TP.Models.Entities;
 
 namespace ProgramacionWeb3TP.Controllers{
 
@@ -83,38 +83,45 @@ namespace ProgramacionWeb3TP.Controllers{
         [HttpPost]
         [ValidateAntiForgeryToken]  //Para prevenir ataques CSRF
         public ActionResult RegistrarUsuario([Bind(Include = "Nombre, Apellido, Email, Contrasenia")] Usuario usuario) {
-            if (ModelState.IsValid) {
-                String nombre = usuario.Nombre;
-                String apellido = usuario.Apellido;
-                String email = usuario.Email;
-                String contrasenia = usuario.Contrasenia;
-                String contrasenia2 = Request["Contrasenia2"];
+            var encodedResponse = Request.Form["g-Recaptcha-Response"];
+            var isCaptchaValid = ReCaptchaClass.Validate(encodedResponse);
 
-                //Chequeo en la salida los datos que se recibieron
-                System.Diagnostics.Debug.WriteLine("Datos recibidos del formulario: " + nombre + " " + apellido + " " + email + " "
-                                                         + contrasenia + " " + contrasenia2);
-
-                Usuario user = _usuarioService.registrarUsuario(new Usuario(usuario.Nombre, usuario.Apellido, usuario.Email, usuario.Contrasenia), contrasenia2);
-                string mensajeError = "";
-                if (user != null) {
-                    Session["usuarioSesionEmail"] = user.Email;
-                    Session["usuarioSesionNombre"] = user.Nombre;
-                    Session["usuarioSesionApellido"] = user.Apellido;
-                    Session["usuarioSesionId"] = user.IdUsuario;
-                    return RedirectToAction("Index", "Usuario"); /*redirije al Home*/
-                }
-                else {
-                    //Informar de alguna forma que no se pudo registrar el usuario
-                    mensajeError = _usuarioService.mostrarMensajeDeError();
-                    TempData["Error"] = mensajeError;
-                    return RedirectToAction("Registracion", usuario);
-                }
+            if (!isCaptchaValid) {
+                TempData["Error"] = "El captcha es inválido";
             }
             else {
-                TempData["Error"] = null;
-                return View("Registracion", usuario);
+                if (ModelState.IsValid) {
+                    String nombre = usuario.Nombre;
+                    String apellido = usuario.Apellido;
+                    String email = usuario.Email;
+                    String contrasenia = usuario.Contrasenia;
+                    String contrasenia2 = Request["Contrasenia2"];
+
+                    //Chequeo en la salida los datos que se recibieron
+                    System.Diagnostics.Debug.WriteLine("Datos recibidos del formulario: " + nombre + " " + apellido + " " + email + " "
+                                                             + contrasenia + " " + contrasenia2);
+
+                    Usuario user = _usuarioService.registrarUsuario(new Usuario(usuario.Nombre, usuario.Apellido, usuario.Email, usuario.Contrasenia), contrasenia2);
+                    string mensajeError = "";
+                    if (user != null) {
+                        Session["usuarioSesionEmail"] = user.Email;
+                        Session["usuarioSesionNombre"] = user.Nombre;
+                        Session["usuarioSesionApellido"] = user.Apellido;
+                        Session["usuarioSesionId"] = user.IdUsuario;
+                        return RedirectToAction("Index", "Usuario"); /*redirije al Home*/
+                    }
+                    else {
+                        //Informar de alguna forma que no se pudo registrar el usuario
+                        mensajeError = _usuarioService.mostrarMensajeDeError();
+                        TempData["Error"] = mensajeError;
+                    }
+                }
+                else {
+                    TempData["Error"] = null;
+                }
             }
-        }
+             return View("Registracion", usuario);
+            }
     }
 }
 
