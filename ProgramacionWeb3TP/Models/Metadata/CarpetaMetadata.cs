817300567aaceae55361eae12109f8afcd0fc2d4
ARomina <aldanarominabarraza@gmail.com>
2018-07-07 21:27:43 -0300

    Metadata editada (carpeta y tarea)

--- ProgramacionWeb3TP/Models/Metadata/CarpetaMetadata.cs
+++ ProgramacionWeb3TP/Models/Metadata/CarpetaMetadata.cs
@@ -7,7 +7,13 @@ using System.Web;
 namespace ProgramacionWeb3TP.Models.Metadata
 {
     public class CarpetaMetadata {
+
+        [Required(ErrorMessage = "La carpeta debe tener un nombre")]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
         [Display(Name = "Descripción")]
+        [StringLength(200)]
         public string Descripcion { get; set; }
 
         [Display(Name = "Fecha de Creación")]
--- ProgramacionWeb3TP/Models/Metadata/TareaMetadata.cs
+++ ProgramacionWeb3TP/Models/Metadata/TareaMetadata.cs
@@ -7,10 +7,17 @@ using System.Web;
 namespace ProgramacionWeb3TP.Models.Metadata
 {
     public class TareaMetadata {
+
+        [Required(ErrorMessage = "La tarea debe tener un nombre")]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
         [Display(Name = "Descripción")]
+        [StringLength(200)]
         public string Descripcion { get; set; }
 
         [Display(Name = "Estimado en Horas")]
+        [RegularExpression(@"^\d+\.\d{0,2}$", ErrorMessage = "Formato de horas incorrecto (hasta 2 decimales)")]
         public Nullable<decimal> EstimadoHoras { get; set; }
 
         [Display(Name = "Fecha Fin")]
