169f8cfdf26ddadfe4617c2c2d18fcebdca7cd78
ARomina <aldanarominabarraza@gmail.com>
2018-07-08 17:09:18 -0300

    Se agregó Bundle y minification

---
+++ ProgramacionWeb3TP/App_Start/BundleConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ProgramacionWeb3TP {
+
+    public class BundleConfig {
+        public static void RegisterBundles(BundleCollection bundles) {
+            bundles.Add(new StyleBundle("~/Content/CustomCSS").Include(
+                        "~/Content/customStyles.css",
+                        "~/Content/customTheme.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-1.*"));
+
+            /*bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui*"));*/
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryValidate").Include(
+                        "~/Scripts/jquery.*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            /*bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                       "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));*/
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
